a20f613e2fd929f6027644b2de34b7e250cb7b5f
KoT <Kotto@student.su>
2022-10-01 17:39:23 +0400

    rename task 1.2

--- Task_1.2/Task_1.2.py
+++ Task_1.2/Task_1.2.py
@@ -1,19 +1,29 @@
-# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# Напишите программу, которая принимает на вход координаты точки (X и Y), 
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится)
 
-def Predicatum(x):
-    side1 = not (x[0] or x[1] or x[2])
-    side2 = not x[0] and not x[1] and not x[2]
-    result = side1 == side2
-    return result
-def inputNumbers(x):
-    coordinates = ["x", "y", "z"]
-    a = []
+def Input_print(x):
+    num = [0] * x
     for i in range(x):
-        a.append(input(f"Введите значение {coordinates [i]}: "))
-    return a
-print (f"Проверим истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z")
-veracity = inputNumbers(3)
-if Predicatum(veracity) == True:
-    print(f"*Указанное утверждение истинно*")
-else:
-    print(f"*Указанное утверждение ложно*")
+        check = False
+        while not check:
+            try:
+                number = float(input(f"Введите значение координаты {i+1}: "))
+                num [i] = number
+                check = True
+                if num [i] == 0:
+                    check = False
+                    print("Ошибка! X ≠ 0 и Y ≠ 0!")
+            except ValueError:
+                print("Ошибка! Значения координат не должны быть буквами или знаками")
+    return num
+def Verification (num):
+    text = 4
+    if num [0] > 0 and num [1] > 0:
+        text = 1
+    elif num [0] < 0 and num [1] > 0:
+        text = 2
+    elif num [0] < 0 and num [1] < 0:
+        text = 3
+    print(f"Точка с указанными координатами находится в {text} четверти плоскости")
+koordinate = Input_print(2)
+Verification (koordinate)
